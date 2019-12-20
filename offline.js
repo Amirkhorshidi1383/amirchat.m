942e934863fbf76c98dc27efe688571b721a0c63
Amir <amireisakhorshidi.fanavary2018@gmail.com>
2019-12-20 15:15:03 +0330

    upload app

---
+++ offline.js
@@ -0,0 +1,15 @@
+﻿{
+	"version": 1576841100,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/logo-sheet0.png",
+		"images/title-sheet0.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
